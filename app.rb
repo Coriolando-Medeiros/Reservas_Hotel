adf3b96d19e782e5f2e9b167678210785ff7de77
Coriolando <coriolando.medeiros@gmail.com>
2024-09-11 11:14:55 -0300

    feat(app): criar a funcionalidade de menu inicial

---
+++ app.rb
@@ -0,0 +1,30 @@
+puts "Bem-vindo"
+puts "Sistema de reservas"
+
+loop do
+  puts "1 - Fazer reserva"
+  puts "2 - Ver reservas"
+  puts "3 - Ver clientes"
+  puts "0 - Sair"
+  print "Opção: "
+  opcao = gets.chomp.to_i
+
+  if opcao == 1
+    require_relative 'reserva'
+    reserva = Reserva.new
+    reserva.reservar
+  elsif opcao == 2
+    require_relative 'reserva'
+    reserva = Reserva.new
+    reserva.ver_reservas
+  elsif opcao == 3
+    require_relative 'cliente'
+    reserva = Cliente.new
+    reserva.ver_clientes
+  elsif opcao == 0
+    puts "Saindo..."
+    return
+  else
+    puts "Opção inválida! tente novamente"
+  end
+end
