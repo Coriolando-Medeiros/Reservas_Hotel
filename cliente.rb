e78f5dc0d2954fde37e2796d4bf76d73fd13e2f0
Coriolando <coriolando.medeiros@gmail.com>
2024-09-11 10:02:12 -0300

    refactor(cliente): refatorar a função de informações do cliente para reduzir a quantidade de arquivos txt

--- cliente.rb
+++ cliente.rb
@@ -21,26 +21,20 @@ class Cliente
     @quarto = quarto.quarto
     
     if @quarto
-      File.open("clientes.txt", "a") do |arquivo|
-        arquivo.puts "Nome: #{@nome} | Endereço: #{@rua}, #{@numero} - #{@bairro} - #{@cidade} - #{@estado} | Telefone: #{@telefone} | Quarto: #{@quarto}"
-      end
+      @clientes = {
+        nome: @nome,
+        endereco: "#{@rua}, #{@numero} - #{@bairro} - #{@cidade} - #{@estado}",
+        telefone: @telefone,
+        quarto: @quarto
+      }
+    else
+      puts "Sem informações para atribuir!"
     end
+
   end
 
-  def ver_clientes
-    
-    if File.exist?("clientes.txt") && !File.zero?("clientes.txt")
-      File.open("clientes.txt") do |arquivo|
-        arquivo.each_line do |linha|
-          puts linha
-        end
-      end
-    else
-      puts "Arquivo inexistente ou vazio!"
-    end
-  end
+  
 end
 
 cliente = Cliente.new
 cliente.informacoes
-cliente.ver_clientes
